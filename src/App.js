da5127d710bd528be7e0a8994ff10d7e83ee9e8e
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2021-01-13 03:07:35 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
+				<MouseComponent />
 				{/* one more component missing */}
 			</div>
 		);
